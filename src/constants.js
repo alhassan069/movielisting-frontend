7c3ee481f908b5f9746a1746b67ccc089214b72b
Alimul Hassan <alhassan069@gmail.com>
2023-02-20 06:29:25 +0530

    changed the api url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 // This is the AWS deployed backend-link
-const HomeApiUrl = "http://3.110.176.201/"
+// const HomeApiUrl = "http://3.110.176.201/"
 // Use this when running on localhost
-// const HomeApiUrl = "http://localhost:5000/" 
+const HomeApiUrl = "http://localhost:5000/"
 export default HomeApiUrl;
